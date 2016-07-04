b054a10dd0a32bddaafb3331f0313c2d96083f62
Peter Brown <phbrown@acm.org>
2016-07-04 00:10:29 -0400

    Now displaying the videos for the matching signs.
    Trying to regularize the display of the handshape div's; a good
    deal remains to be done there.

--- functions.js
+++ functions.js
@@ -15,23 +15,42 @@ function parseNumHands(val) {
 }
 
 function valueOrUndefined(value) {
+    result = value;
     if ((value === "") || (value === null)) {
-	return undefined;
+	result = undefined;
     }
-    else return value;
+    else if (typeof(value) === "string") {
+	result = value.toLowerCase();
+    }
+    return result;
 }
 
-function parseHandShape(numHands, hand0start, hand0end,
-			hand1start, hand1end) {
+function parseOneHandShape(startSpec, endSpec) {
+    var start = valueOrUndefined(startSpec);
+    var end = valueOrUndefined(endSpec);
+    return [start, end];
+}
+
+function parseHandShape(numHands) {
     var result = [];
-    var start = valueOrUndefined(hand0start);
-    var end = valueOrUndefined(hand0end);
-    result.push([start, end]);
-    
+    var hand0start = $('#hand0shape0').val();
+    var hand0end = $('#hand0shape1').val();
+    if ($('#hand0shape1div').is(':hidden')) {
+	hand0end = hand0start;
+    }
+    result.push(parseOneHandShape(hand0start, hand0end));
+
     if (numHands > 1) {
-	start = valueOrUndefined(hand1start);
-	end = valueOrUndefined(hand1end);
-	result.push([start, end]);
+	var hand1start = $('#hand1shape0').val();
+	var hand1end = $('#hand1shape1').val();
+	if ($('#hand1shape0div').is(':hidden')) {
+	    hand1start = hand0start;
+	    hand1end = hand0end;
+	}
+	else if ($('#hand1shape1div').is(':hidden')) {
+	    hand1end = hand1start;
+	}
+	result.push(parseOneHandShape(hand1start, hand1end));
     }
     
     return result;
@@ -56,9 +75,9 @@ function parsePositionXYZ(loc, inFront) {
 	result[0] = result[1] = result[2] = undefined; break; 
     case 'forehead': // [0, 6, z]
 	result[1] = 6; break;
-    case 'L-eye': // [1, 5, z]
+    case 'l-eye': // [1, 5, z]
 	result[0] = 1; result[1] = 5; break;
-    case 'R-eye': // [-1, 5, z]
+    case 'r-eye': // [-1, 5, z]
 	result[0] = -1; result[1] = 5; break;
     case 'temple': // [-2, 5, -1]
 	result[0] = -2; result[1] = 5; result[2] = -1; break;
@@ -66,9 +85,9 @@ function parsePositionXYZ(loc, inFront) {
 	result[0] = -2; result[1] = 4; result[2] = -2; break;
     case 'nose': // [0, 4, z]
 	result[1] = 4; break;
-    case 'L-cheek': // [2, 3, z]
+    case 'l-cheek': // [2, 3, z]
 	result[0] = 2; result[1] = 3; break;
-    case 'R-cheek': // [-2, 3, z]
+    case 'r-cheek': // [-2, 3, z]
 	result[0] = -2; result[1] = 3; break;
     case 'mouth': // [0, 3, z]
 	result[1] = 3; break;
@@ -76,9 +95,9 @@ function parsePositionXYZ(loc, inFront) {
 	result[1] = 2; break;
     case 'neck': // [0, 1, z]
 	result[1] = 1; break;
-    case 'L-shoulder': // [1, 0, z]
+    case 'l-shoulder': // [1, 0, z]
 	result[0] = 1; break;
-    case 'R-shoulder': // [-1, 0, z]
+    case 'r-shoulder': // [-1, 0, z]
 	result[0] = -1; break;
     case 0:
     case 'sightline':
@@ -130,11 +149,7 @@ function Sign() {
     this.hands = parseNumHands(formElt.numhands.value);
     //alert(this.hands);
 
-    this.handshape = parseHandShape(this.hands,
-	                            $('#hand0shape0').val(),
-	                            $('#hand0shape1').val(),
-	                            $('#hand1shape0').val(),
-	                            $('#hand1shape1').val());
+    this.handshape = parseHandShape(this.hands);
     //alert(JSON.stringify(this.handshape));
     
     this.position = parsePositionText($('#loc0').val(),
@@ -155,11 +170,12 @@ function Sign() {
 var CLEARLY_DIFFERENT = 1000;
 
 function compareHandshapes(shape1, shape2) {
+    //alert('Handshapes: ' + JSON.stringify(shape1) + ', ' + JSON.stringify(shape2));
     var diff = CLEARLY_DIFFERENT;
-    if (shape1 === shape2) {
+    if (shape1 == shape2) {
 	diff = 0;
     }
-    else if (shape1 === 'undefined' || shape2 === 'undefined') {
+    else if (shape1 === undefined || shape2 === undefined) {
 	diff = 1;
     }
     else if (handshapes[shape1].group === handshapes[shape2].group) {
@@ -171,7 +187,7 @@ function compareHandshapes(shape1, shape2) {
 function cmpVec3(v1, v2, tolerance) {
     var diff = 0;
     for (var i = 0; i < 3; i++) {
-	var d = Math.abs(v1[i] - v2[1]);
+	var d = Math.abs(v1[i] - v2[i]);
 	if (d > tolerance) {
 	    diff = CLEARLY_DIFFERENT;
 	}
@@ -186,10 +202,13 @@ function comparePositions(pos1, pos2) {
     var pt1 = parsePositionXYZ(pos1, false);
     var pt2 = parsePositionXYZ(pos2, false);
     return cmpVec3(pt1, pt2, 2);
-    
 }
 
 function compareSigns(sign1, sign2) {
+    // if (sign1.hands === 1.5 && sign2.hands === 1.5) {
+    // 	alert('sign1: ' + JSON.stringify(sign1));
+    // 	alert('sign2: ' + JSON.stringify(sign2));
+    // }
     var result = 2 * Math.abs(sign1.hands - sign2.hands); // 0, 1, or 2
     if (result > 1) { // One-handed vs. two-handed 
 	result = CLEARLY_DIFFERENT;
@@ -227,6 +246,10 @@ function compareSigns(sign1, sign2) {
 	}
 	result += cmpVec3(sign1.motion.dir, sign2.motion.dir, 1);
     }
+
+    // if (sign1.hands === 1.5 && sign2.hands === 1.5) {
+    // 	alert('Final result: ' + result);
+    // }
     return result;
 }
 
@@ -287,9 +310,12 @@ var handleImgClick = function(event) {
 }
 
 function fillHandshapeDiv(divElt) {
-    $(divElt).append('<h4>Beginning:</h4>');
     var handStr = divElt.id.substring(0, 5);
-    $(divElt).append(makeHandshapeSelect(handStr + 'shape0'));
+    
+    var startDiv = document.createElement('div');
+    startDiv.id = handStr + 'shape0div';
+    $(startDiv).append('<h4>Beginning:</h4>');
+    $(startDiv).append(makeHandshapeSelect(handStr + 'shape0'));
 
     var endButton = document.createElement('button');
     var showText = 'Ending handshape is different';
@@ -298,16 +324,18 @@ function fillHandshapeDiv(divElt) {
     endButton.name = handStr + 'EndButton';
     endButton.id = endButton.name;
     $(endButton).text(showText);
-    divElt.appendChild(endButton);
+    startDiv.appendChild(endButton);
+    divElt.appendChild(startDiv);
 
     var endDiv = document.createElement('div');
+    endDiv.id = handStr + 'shape1div';
     $(endDiv).append('<h4>Ending:</h4>');
-    $(endDiv).append(makeHandshapeSelect('hand0shape1'));
+    $(endDiv).append(makeHandshapeSelect(handStr + 'shape1'));
     $(endDiv).hide();
 
     var endButtonFn = function() {
 	$(endDiv).toggle();
-	if ($(endButton).text() == showText) {
+	if ($(endButton).text() === showText) {
 	    $(endButton).text(hideText);
 	}
 	else {
@@ -315,15 +343,83 @@ function fillHandshapeDiv(divElt) {
 	}
     };
     endButton.onclick = endButtonFn;
+
+    // This doesn't trigger properly.  Why Not?
+    var endButtonReset = function() {
+	$(endButton).text(showText);
+	$(endDiv).hide();
+    }
+    endButton.onreset = endButtonReset;
     
     divElt.appendChild(endDiv);
 }
 
-function evalGuess() {
-    var sign = Sign();
+function makeHand1Button() {
+    var hand1Button = document.createElement('button');
+    hand1Button.type = 'button';
+    hand1Button.id = 'showHand1ShapeButton';
+    $(hand1Button).data('showText', 'Non-dominant handshape is different');
+    $(hand1Button).data('hideText', 'Non-dominant handshape is the same');
+    $(hand1Button).text($(hand1Button).data('showText'));
 
+    var hand1ButtonFn = function () {
+	if ($(hand1Button).text() === $(hand1Button).data('showText')) {
+	    $('#hand1shape0div').show();
+	    $('#hand1shape1div').hide();
+	    $(hand1Button).text($(hand1Button).data('hideText'));
+	}
+	else {
+	    $('#hand1shape0div').hide();
+	    $('#hand1shape1div').hide();
+	    $(hand1Button).text($(hand1Button).data('showText'));
+	}      
+    };
+    hand1Button.onclick = hand1ButtonFn;
+
+    return hand1Button;
+}
+
+function makeSignRow(sign) {
+    var row = document.createElement('tr');
+    row.id = sign.sign + '-row';
+    row.className = 'signrow';
+    
+    var th = document.createElement('th');
+    th.id = sign.sign + '-title';
+    th.className = 'signtitle';
+    $(th).text(sign.sign);
+    row.appendChild(th);
+    
+    var signSrc = document.createElement('source');
+    signSrc.src = sign.video;
+    signSrc.type = 'video/mp4';
+    
+    var signVid = document.createElement('video');
+    signVid.id = sign.sign + '-video';
+    signVid.className = 'signvideo';
+    signVid.appendChild(signSrc);
+    $(signVid).attr('controls', 'on');
+
+    var td = document.createElement('td');
+    td.appendChild(signVid);
+    row.appendChild(td);
+    
+    return row;
+}
+
+function showSigns(signsToShow) {
+    $('#results').empty();
+    signsToShow.forEach(function(item, idx, arrayVar) {
+	$('#results').append(makeSignRow(item));
+    });
+}
+
+function evalGuess() {
+    var guess = new Sign();
+    alert(JSON.stringify(guess));
+    
     for (var s = 0; s < signs.length; s++) {
-	signs[s].diff = compareSigns(sign, signs[s]);
+	signs[s].diff = compareSigns(guess, signs[s]);
     }
     var possibles = signs.filter(function(elt, idx, arr) {
 	return (elt.diff < CLEARLY_DIFFERENT);
@@ -332,13 +428,22 @@ function evalGuess() {
 	return a.diff - b.diff;
     })
     
+    //alert(JSON.stringify(possibles));
+
     // Display the possibilities
-    
+    showSigns(possibles);
 }
 
 $(document).ready( function() {
     var changeHands = function() {
 	var classToShow = $("#numhands option:selected").val();
+	if (classToShow === 'one') {
+	    $('#hand1shape0div').hide();
+	    $('#hand1shape1div').hide();
+	    $('#showHand1ShapeButton').text(
+		$('#showHand1ShapeButton').data('showText'));
+	    $('#hand1div').hide();
+	}
 	//alert(classToShow);
 	$(".one, .moving, .two").hide();
 	$('.' + classToShow).show();
@@ -346,7 +451,9 @@ $(document).ready( function() {
     $("#numhands").change(changeHands);
 
     fillHandshapeDiv(document.getElementById('hand0div'));
+    $("#hand1div").append(makeHand1Button());
     fillHandshapeDiv(document.getElementById('hand1div'));
+    $('#hand1div').hide();
     
     $("select.handshape").imagepicker({
 	/*hide_select: false,*/
