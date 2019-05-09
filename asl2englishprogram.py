7d07ac8324dcc0ec9134c4ef48aaaaa1f7198b06
Turner <ABTurner002@converse.edu>
2019-05-09 15:45:35 -0400

    Fixed all iterations, only thing left is insert statements for regions, handshapes

--- asl2englishprogram.py
+++ asl2englishprogram.py
@@ -49,52 +49,88 @@ def readData():
         
         
         """print frontregions DICT img, width, height, regions"""
-        print(data['frontregions'])
+        #print(data['frontregions'])
         """print sideregions DICT, img, width, height, regions"""
-        print(data['sideregions']) #json.dumps('sideregions')
+        #print(data['sideregions']) #json.dumps('sideregions')
         return data
     
 def makeSQL(data):
-    sql = []
+    sql = ''
     for p in data['handshapes']:
         if p != "null":
-            print(p)
-            insert = 'insert into HandShape (id, shape, group, img) values ' \
-                + "({0:d}, '{1}', '{2}', '{3}');".format(p['id'], p['shape'],
+            #print(p)
+            insert = 'insert into HandShape (shape, group, img) values ' \
+                + "('{0}', '{1}', '{2}');".format(p['shape'],
                                                 p['group'], p['img'])
-            sql.append(insert)
+            print(insert)
+            sql += insert
 
-    for p in data['shapegroups']:
-        print(p)
-        insert = 'insert into Shapegroup (p) values ' \
-            + "({0:d});".format(p)
-        sql.append(insert)
 
-    for p in data['frontregions']:
-        print(p)
-        frontvalues = [p]
-        i = 0
-        while (i < len(frontvalues)):
-            insert = 'insert into Regions (id, name, image, X, Y, Z, elt, pts, cx, cy, rx, ry ' \
-            #frontvalues[i]
-            i += 1
-            sql.append(insert)
-        #iterate the keys / values ; need some kind of iteration i = ___
-    for p in data['sideregions']:
-        print(p)
-        sidevalues = [p]
-        i = 0
-        while (i < len(frontvalues)):
-            insert = 'insert into Regions (id, name, image, X, Y, Z, elt, pts, cx, cy, rx, ry ' \
-            #sidevalues[i]
-            i += 1
-            sql.append(insert)
+    for name in data['frontregions']['regions']:
+        image = data['frontregions']['image']
+        #print('\n', name, data['frontregions']['regions'][name])
+        p = data['frontregions']['regions'][name]
+        #print(p)
+        if p['elt'] == 'polygon':
+            if len(p['xyz']) == 3:
+                insert = 'insert into Regions (name, image, X, Y, Z, elt, pts) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, {4:.1f}, 'polygon', '{5}');".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['xyz'][2], 
+                            p['points'])
+            else:
+                insert = 'insert into Regions (name, image, X, Y, elt, pts) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, 'polygon', '{4}');".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['points'])
+        elif p['elt'] == 'ellipse':
+            if len(p['xyz']) == 3:
+                insert = 'insert into Regions (name, image, X, Y, Z, elt, cx, cy, rx, ry) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, {4:.1f}, 'ellipse', {5:d}, {6:d}, {7:d}, {8:d});".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['xyz'][2], 
+                            p['c'][0], p['c'][1], p['r'][0], p['r'][1])
+            else:
+                insert = 'insert into Regions (name, image, X, Y, elt, cx, cy, rx, ry) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, 'ellipse', {4:d}, {5:d}, {6:d}, {7:d});".format(
+                            name, image, p['xyz'][0], p['xyz'][1], 
+                            p['c'][0], p['c'][1], p['r'][0], p['r'][1])
+        print(insert) 
+        sql += insert
+
+    for name in data['sideregions']['regions']:
+        image = data['sideregions']['image']
+        #print('\n', name, data['sideregions']['regions'][name])
+        p = data['sideregions']['regions'][name]
+        #print(p)
+        if p['elt'] == 'polygon':
+            if len(p['xyz']) == 3:
+                insert = 'insert into Regions (name, image, X, Y, Z, elt, pts) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, {4:.1f}, 'polygon', '{5}');".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['xyz'][2], 
+                            p['points'])
+            else:
+                insert = 'insert into Regions (name, image, X, Y, elt, pts) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, 'polygon', '{4}');".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['points'])
+        elif p['elt'] == 'ellipse':
+            if len(p['xyz']) == 3:
+                insert = 'insert into Regions (name, image, X, Y, Z, elt, cx, cy, rx, ry) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, {4:.1f}, 'ellipse', {5:d}, {6:d}, {7:d}, {8:d});".format(
+                            name, image, p['xyz'][0], p['xyz'][1], p['xyz'][2], 
+                            p['c'][0], p['c'][1], p['r'][0], p['r'][1])
+            else:
+                insert = 'insert into Regions (name, image, X, Y, elt, cx, cy, rx, ry) values '
+                insert += "('{0}', '{1}', {2:.1f}, {3:.1f}, 'ellipse', {4:d}, {5:d}, {6:d}, {7:d});".format(
+                            name, image, p['xyz'][0], p['xyz'][1], 
+                            p['c'][0], p['c'][1], p['r'][0], p['r'][1])
+        print(insert) 
+        sql += insert
+
+
+
     return sql
-    
-    
 
 
-        
+
+
 def create_connection(dbfile):
     """ create a database connection to a SQLite database for asl2english """
     try:
@@ -118,10 +154,10 @@ def create_connection(dbfile):
 def main():
     data = readData()
     sqlStatements = makeSQL(data) # Result is a long list of INSERT statements
-    print(sqlStatements)
-    exit()
     try:
         conn = create_connection("asl2english-master\\asl2english.sqlite")
+        cur = conn.cursor()
+        cur.execute(sqlStatements)
     finally:
         conn.close()
     
@@ -131,13 +167,13 @@ def main():
 if __name__ == '__main__':
     main()
     
-SELECT
-*
-FROM
-Handshape;
+#~ SELECT
+#~ *
+#~ FROM
+#~ Handshape;
  
-SELECT
-*
-FROM
-Regions;
+#~ SELECT
+#~ *
+#~ FROM
+#~ Regions;
  
